b1a09712e766af0204aa8fd5e16a3ff191206f3a
panameraxx <90213411+panameraxx@users.noreply.github.com>
2024-12-10 10:08:57 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// Определить колво элементов в одномерном массиве с явной инициализацией
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = { 1, 21, 1, 2, 3 };
+            int size = omas.Length;
+            Console.WriteLine($"Размер: {size}");
+            Console.ReadKey();
+        }
+    }
+}
