9df352b621551d02dc32d9ed80d463bbde118a1b
panameraxx <90213411+panameraxx@users.noreply.github.com>
2024-12-10 10:11:07 +0200

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+     class Program
+    {
+      /// <summary>
+      /// Найти минимальное или максимальное значение в массиве 
+      /// </summary>
+     
+        static void Main(string[] args)
+        {
+            int[] omas = { -8, 6, 9, 10 };
+            int max = omas.Max();
+            int min = omas.Min();
+            Console.WriteLine($"минимальное = {min} \n максимальное = {max}");
+            Console.ReadKey();
+        }
+    }
+}
