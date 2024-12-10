05331b175da60d2f2fa7c56de03b6a21f23f9460
panameraxx <90213411+panameraxx@users.noreply.github.com>
2024-12-10 10:11:51 +0200

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    /// <summary>
+    /// Сортировка массива по возрастанию и выполнить реверс 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fruit = { 4, 3, 6,7 };
+            Array.Sort(fruit);
+            Console.WriteLine(fruit);
+            Array.Reverse(fruit);
+            Console.WriteLine(fruit);
+            Console.ReadKey();
+        }
+    }
+}
